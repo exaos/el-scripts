d4bb74f0b5033f903164057bfa808a4e4554a890
Exaos Lee <Exaos.Lee@gmail.com>
2010-01-12 07:52:04 +0800

    Alter app-rc.sh using user-first principle

--- app-envsel.py
+++ app-envsel.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 '''
-Usage: env-selector <command> [app] [options]
+Usage: app-envsel <command> [app] [options]
 
 Commands:
-  init
-  register
-  unregister
-  remove
-  list
-  set
-  unset
+  init       ---  setup the environment for app-envsel
+  register   ---  register verion to certain application
+  remove     ---  remove one or all versions of certain application
+  list       ---  list versions of certain application
+  set        ---  choose one version for certain application
+  unset      ---  unset one version for certain application
+
+Author: Exaos Lee <Exaos.Lee(at)gmail.com>, Jan. 2010
 '''
 import os, sys
 import os.path as path
-from base64 import b64decode
-from zlib   import decompress
+from base64   import b64decode
+from zlib     import decompress
 
 # Check the data path
 app_data_sys = "/var/lib/app-envsel/data"
 app_env_sys  = "/var/lib/app-envsel/env"
 app_data_user= path.join(os.environ['HOME'],'.config/app-envsel/data')
 app_env_user = path.join(os.environ['HOME'],'.config/app-envsel/env')
+# app-rc.sh
 app_rc_enc='''
 eJyVVGFv2jAQ/Vz/isOgikgLKfvYLpNQybRJrJ0W9mEaVRqIAUtgR7ahYyz/fT4HkpTBpAkp
 2Jd3757vndNuBVMugmmql6QN9zLfKb5YGujOPHh707+B6GcqNYwYg3du2bPLbmq8xTrlq95M
@@ -37,7 +39,7 @@ HJhL1B1kEg73xY0LBoT1q7LlrFcum9goKUMHl6BxcaF5KWvYsZXQ7AkhfwDvIqFK
 app_rc_real=decompress(b64decode(app_rc_enc.replace('\n','')))
 
 class app_envdata:
-    _start_sh = dict()
+    _start_sh  = dict()
     _isChecked = False
     _isSystem  = False
 
@@ -129,8 +131,67 @@ class app_envsel(app_envdata):
     def set_env(self,ename):   pass
     def unset_env(self,ename): pass
 
-def my_main(a_env_sel, isSYS = False):
-    pass
+
+def cmd_process(args):
+    arglen = len(args)
+    if arglen==0 or args[0]=="":
+        print __doc__
+        return
+
+    # init
+    if args[0]=="init":
+        if arglen<2:
+            print "Usage: app-envsel init <-s/-u>"
+            print "  -s   ---  system-wide initial"
+            print "  -u   ---  user-wide initial"
+        elif args[1]=="-s": # system-wide init
+            pass
+        else:  # user-wide init
+            pass
+    # register/reg
+    elif args[0]=="register" or args[0]=="reg":
+        if arglen<3:
+            print "Usage: app-envsel register <app> <path-to-ver-sh>"
+            return
+        else: # register <app> <path-to-ver-sh>
+            pass
+    # remove/rm
+    elif args[0]=="remove" or args[0]=="rm":
+        if arglen<2:
+            print "Usage: app-envsel remove <app> <option>"
+            print "Option:"
+            print "   <ver>   --- remove specfic version"
+            print "   -a      --- remove all versions"
+        else:
+            pass
+    # list/ls
+    elif args[0]=="list" or args[0]=="ls":
+        if arglen<2 or args[1]=="-a": # List all apps
+            pass
+        else: # List <app>
+            pass
+    # set
+    elif args[0]=="set":
+        if arglen<3:
+            print "Usage: app-envsel set <app> [-s] <ver>"
+            print "Option: -s  --- set the system-wide version"
+        else:
+            if arglen>3 and args[2]=="-s": # system-wide
+                pass
+            else: # User-wide
+                pass
+    # unset
+    elif args[0]=="unset":
+        if arglen<3:
+            print "Usage: app-envsel unset <app> [-s] <ver>"
+            print "Option: -s  --- unset the system-wide version"
+        else:
+            if arglen>3 and args[2]=="-s": # system-wide
+                pass
+            else: # User-wide
+                pass
+    else:
+        print __doc__
 
 if __name__=='__main__':
     import sys
@@ -140,6 +201,6 @@ if __name__=='__main__':
         app_data_dir = app_data_user
     else:
         print "** ERROR **: Neither system-wide nor user-wide data directory exists!"
+        print "You need to run: app-envsel init [-s|-u]"
         sys.exit(1)
-    #print "Reading data from %s"%(app_data_dir)
-    print app_rc_real
+    cmd_process(sys.argv[1:])
--- app-rc.sh
+++ app-rc.sh
@@ -9,9 +9,6 @@ AE_SETD_SYS=/var/lib/app-envsel/env
 AE_DATA_USER=$HOME/.config/app-envsel/data
 AE_SETD_USER=$HOME/.config/app-envsel/env
 
-[ -d ${AE_DATA_SYS} ] && AE_DATA=${AE_DATA_SYS}
-[ -z "${AE_DATA}"   ] && AE_DATA=${AE_DATA_USER}
-
 set_env()
 {
     appn=$1
@@ -21,18 +18,23 @@ set_env()
     if [ -z "$appv" ] && [ -f ${AE_SETD_SYS}/$appn ]; then
 	appv=$(sed /\#/d ${AE_SETD_SYS}/$appn | sed -n 1p)
     fi
-    echo -n "Setting $appn: ${AE_DATA}/${appn}_$appv.sh .. "
-    if [ -f ${AE_DATA}/${appv}.sh ]; then
-	. ${AE_DATA}/${appn}_${appv}.sh
+    echo -n "Setting $appn:${appv} -- "
+    if [ -f ${AE_DATA_USER}/${appn}_${appv}.sh ]; then
+	echo -n "${AE_DATA_USER}/${appn}_$appv.sh .. "
+	. ${AE_DATA_USER}/${appn}_${appv}.sh
+	echo "[DONE]"
+    elif [ -f ${AE_DATA_SYS}/${appn}_${appv}.sh ]; then
+	echo -n "${AE_DATA_SYS}/${appn}_$appv.sh .. "
+	. ${AE_DATA_SYS}/${appn}_${appv}.sh
 	echo "DONE!"
     else
-	echo "FAILED"
+	echo "[FAILED]"
     fi
     unset appn appv
 }
 
-if [ ! -d ${AE_DATA} ]; then
-    echo "Data directory is not set for \"app-envsel\""
+if [ ! -d ${AE_DATA_SYS} -a ! -d ${AE_DATA_USER} ]; then
+    echo "WARNING: Data directory is not ready for \"app-envsel\"!"
 else
     [ -d ${AE_SETD_SYS} ]  && app_env_sys=$(ls -A ${AE_SETD_SYS})
     [ -d ${AE_SETD_USER} ] && app_env_user=$(ls -A ${AE_SETD_USER})
