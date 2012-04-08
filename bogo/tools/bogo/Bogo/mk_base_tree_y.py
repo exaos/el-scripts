5991f3e1a65585a43f01d85b5b3836b8cb6c7fc0
Exaos Lee <Exaos.Lee@gmail.com>
2012-04-08 17:00:15 +0800

    import old project: bogo

---
+++ bogo/tools/bogo/Bogo/mk_base_tree_y.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import yaml
+import os
+import os.path as path
+
+d_tree = yaml.load(open("base_tree.yaml"))
+
+def gen_tree_list(dtree,parent=''):
+    t_list = []
+    if type(dtree)==unicode:
+        return [path.join(parent,dtree),]
+    elif type(dtree)==list:
+        for d in dtree: t_list.extend(gen_tree_list(d,parent))
+    elif type(dtree)==dict:
+        for d in dtree.keys():
+            t_list.extend(gen_tree_list(dtree[d],path.join(parent,d)))
+    else: pass
+    return t_list
+
+def mk_base_tree(base='',mode=0755):
+    for d in gen_tree_list(d_tree):
+        dname = path.join(base,d)
+        print("Creating %s ..."%dname)
+        os.makedirs(dname,mode)
+
