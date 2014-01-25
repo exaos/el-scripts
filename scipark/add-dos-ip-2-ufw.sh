adf905d732694ad8545da798997ce126d3a91bad
Exaos Lee <Exaos.Lee@gmail.com>
2014-01-25 09:55:43 +0800

    SciPark: Add white IP list to add-dos-ip-2-ufw.sh ; Update DOS IP list

--- scipark/add-dos-ip-2-ufw.sh
+++ scipark/add-dos-ip-2-ufw.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ## check ROOT privilege
 if [[ $EUID -ne 0 ]]; then
     echo "**ERROR** This script should be run using sudo or as the root user"
@@ -7,28 +6,17 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 ## 
-
 LOG_DIR=/var/log/mod_evasive/
 IP_LIST=`ls -1 ${LOG_DIR} | awk -F'-' '{ print $2 }' | sort -n`
 UFW_DENY_LIST=`ufw status | grep "Anywhere" | grep "/tcp"`
 
-function is_ip_in_ufw()
-{
-    is_in=`echo ${UFW_DENY_LIST} | grep "$1/tcp"`
-    if [[ X"${is_in}" == X ]]; then
-	echo "no"
-    else
-	echo "yes"
-    fi
-}
+WHITE_IPS=()
 
-function add_ip_to_ufw()
+function add_ip_to_ufw ()
 {
-    if [[ `is_ip_in_ufw $1` == "no" ]]; then
+    if [[ ( ${IP_LIST} =~ $1 ) && ( ! ${WHITE_IPS[@]} =~ $1 ) && ( ! ${UFW_DENY_LIST} =~ $1 ) ]]; then
 	echo "adding $1 to ufw deny list ..."
 	ufw insert 10 deny proto tcp from $1
-    else
-	echo "$1 already added"
     fi
 }
 
