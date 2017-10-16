f72fd217767586aa3e0371e5bc747882860cb932
Exaos Lee <Exaos.Lee@gmail.com>
2017-10-16 14:05:17 +0800

    Add scripts: for host-memes; for py-envs

---
+++ host-memes/mcnp5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MCNP=/opt/sci/bin/mcnp5
+DATAPATH=/opt/sci/mcnpxs/mcnp5-CCC710
+
+${MCNP} $@
---
+++ host-memes/mcnpx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MCNP=/opt/sci/bin/mcnpX
+DATAPATH=/opt/sci/mcnpxs/mcnpx-2.5
+
+${MCNP} $@
